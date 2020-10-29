f424bef3b0012cee413320f57432dcccb86cb63d
Sumant <sbagri.sumant@gmail.com>
2020-10-29 14:04:47 +0800

    Update

--- pyenv-installer.sh
+++ pyenv-installer.sh
@@ -92,7 +92,7 @@ if ! command -v pyenv 1>/dev/null; then
   esac
 
   profile='${profile/#\~/$HOME}'
-  echo "Profile is: '${profile}'" >&1
+  echo "Expanded profile is: '${profile}'" >&1
 
   { case "$shell" in
     fish )
